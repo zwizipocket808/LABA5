4682ca2c1513c8a0dc80cba0c3d5acdb99d92b9b
zwizipocket808 <gv.pa@mail.ru>
2026-05-14 15:59:01 +0500

    Update LABA5.py

--- LABA5.py
+++ LABA5.py
@@ -5,15 +5,22 @@ seq1 = [1, 2]
 seq2 = ['a', 'b']
 seq3 = [True, False]
 
-
-combinations = itertools.product(seq1, seq2, seq3)
-
-# Применяем map
-mapped = map(lambda combo: f"{combo[0]}-{combo[1]}-{combo[2]}", combinations)
+def combo_generator (s1, s2, s3):
+    for a in s1:
+        for b in s2:
+            for c in s3:
+                yield ( a, b, c)
+                
+combinations = combo_generator(seq1, seq2, seq3)
+mapped = map(lambda combo: f"{combo [0]}-{combo[1]}-{combo[2]}", combinations)
 
 total = len(seq1) * len(seq2) * len(seq3)
-print(f"Общее число комбинаций: {total}")
+print(f"Общее число кеомбинаций: {total}")
 
-print("Результат применения map (строка):")
-for item in list(mapped)[:5]:
+print ("Результат применения map (первые 5 строк):")
+
+from itertools import islice 
+for item in islice ( mapped, 5):
     print(item)
+g = combo_generator(seq1, seq2,seq1)
+print ( next(g))
